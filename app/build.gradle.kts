c94a8e4b964857fb060a7fb0da3e1773738cf0e1
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-28 18:08:13 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
+    kotlin("android") version "1.9.22" apply false
+    id("org.jetbrains.compose") version "1.5.11" apply false
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
